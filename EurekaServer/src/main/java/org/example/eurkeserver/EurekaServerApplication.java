cb4338ec4f630690a76300327870a5789aa617a7
Oudompanha123 <oudompanha456@gmail.com>
2026-06-29 09:17:26 +0700

    Fix EurekaServer Gradle build: correct folder name and structure

--- EurekaServer/src/main/java/org/example/eurkeserver/EurekaServerApplication.java
+++ EurekaServer/src/main/java/org/example/eurkeserver/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurkeServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurkeServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
